01fff921909ea823fd2f9bf772da3d1f29eb871a
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2021-01-26 15:52:55 +0100

    Controller connexion à jour

--- lycee_java/src/controller/Controller_connexion.java
+++ lycee_java/src/controller/Controller_connexion.java
@@ -6,6 +6,9 @@ import java.sql.SQLException;
 
 import com.dbconnexion.*;
 
+import appli.Admin_Eleves;
+import appli.Eleves;
+
 public class Controller_connexion {
 	
 	public void connexion(String identifiant, String mdp) throws SQLException {
@@ -17,11 +20,20 @@ public class Controller_connexion {
 		ResultSet resultat = db.Request(cnx, requete);
 		if(resultat != null) {
 			if(resultat.getString(role) == "Admin") {
-				open
+				try { //Connexion en tant qu'Administrateur
+					Admin_Eleves window = new Admin_Eleves();
+					window.open();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-			else {
-				open
+			else { //Connexion en tant que professeur
+				//Co prof
 			}
+			
+		}
+		else { //Erreur identifiant et/ou mot de passe
+			//erreur acces
 		}
 	}
 }
