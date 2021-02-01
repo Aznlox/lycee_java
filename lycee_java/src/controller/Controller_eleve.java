bed57a54e3d7858ccf1eb10a07479c9c8cae18b2
Amine372 <nakhila@orange.fr>
2021-02-01 15:57:49 +0100

    Update Controller_eleve.java

--- lycee_java/src/controller/Controller_eleve.java
+++ lycee_java/src/controller/Controller_eleve.java
@@ -9,11 +9,9 @@ public class Controller_eleve
 
 	public void eleve(String identifiant, String mdp)
 	{
-
-		
-				try
-				{
-					motif window = new motif();
+			try
+			{
+				motif window = new motif();
 					window.open(); //redirection vers notif
 				}
 				catch (Exception e)
