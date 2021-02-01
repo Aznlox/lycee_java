f8bda8277b1ccf3731871831193205d9c0c5cd1b
Amine372 <nakhila@orange.fr>
2021-02-01 14:18:30 +0100

    Update Controller_connexion.java

--- lycee_java/src/controller/Controller_connexion.java
+++ lycee_java/src/controller/Controller_connexion.java
@@ -9,28 +9,36 @@ import com.dbconnexion.*;
 import appli.Admin_Eleves;
 import appli.Eleves;
 
-public class Controller_connexion {
-	
-	public void connexion(String identifiant, String mdp) throws SQLException {
-		
+public class Controller_connexion
+{
+
+	public void connexion(String identifiant, String mdp) throws SQLException
+	{
+
 		Database db = new Database();
 		Connection cnx = db.DbConnexion();
 		String requete = "Select * from utilisateur where identifiant = "+identifiant+" and mdp = "+mdp;
 		String role = "Admin";
 		ResultSet resultat = db.Request(cnx, requete);
 		if(resultat != null) {
-			if(resultat.getString(role) == "Admin") {
-				try { //Connexion en tant qu'Administrateur
+			if(resultat.getString(role) == "Admin")
+			{
+				try
+				{ //Connexion en tant qu'Administrateur
 					Admin_Eleves window = new Admin_Eleves();
 					window.open();
-				} catch (Exception e) {
+				}
+				catch (Exception e)
+				{
 					e.printStackTrace();
 				}
 			}
-			else { //Connexion en tant que professeur
+			else
+			{
+				 //Connexion en tant que professeur
 				//Co prof
 			}
-			
+
 		}
 		else { //Erreur identifiant et/ou mot de passe
 			//erreur acces
