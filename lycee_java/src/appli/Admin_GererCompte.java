9e2e3a4c07dd6ec1d3f0666a6423722abc486769
Amine372 <nakhila@orange.fr>
2021-03-08 16:27:51 +0100

    Update Admin_GererCompte.java

--- lycee_java/src/appli/Admin_GererCompte.java
+++ lycee_java/src/appli/Admin_GererCompte.java
@@ -146,7 +146,7 @@ public class Admin_GererCompte extends Global
 			@Override
 			public void widgetSelected(SelectionEvent e)
 			{
-				String requete = "INSERT into utilisateur (nom, prenom, identifiant, email, mdp, matiere) Values('"+textNom.getText()+"','"+textPrenom.getText()+"','"+textidentifiant.getText()+"','"+textemail.getText()+"','"+textmdp.getText()+"','"+textmatiere.getText()+"','professeur')";
+				String requete = "INSERT into utilisateur (nom, prenom, identifiant, email, mdp, matiere) Values('"+textNom.getString()+"','"+textPrenom.getString()+"','"+textidentifiant.getString()+"','"+textemail.getString()+"','"+textmdp.getString()+"','"+textmatiere.getString()+"','professeur')";
 				boolean message = db.Prepare(cnx, requete);
 				lblErreur.setVisible(message);
 				lblSucces.setVisible(!message);
